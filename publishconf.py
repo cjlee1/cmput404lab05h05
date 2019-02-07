6e8d630e89397873ca162951ab050318919f9dd7
cjlee1 <cjlee@ualberta.ca>
2019-02-07 18:31:09 -0500

    start-part 2

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://github.com/cjlee1/cmput404lab05h05'
+SITEURL = 'https://cjlee1.github.io/cmput404lab05h05/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
